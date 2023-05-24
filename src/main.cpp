5da44f9b53504317edc5807f19c3c26ededcee6b
sarakalinic24 <skalinic48@gmail.com>
2023-05-24 23:35:34 +0200

    face culling and blending

--- src/main.cpp
+++ src/main.cpp
@@ -167,6 +167,12 @@ int main() {
     // -----------------------------
     glEnable(GL_DEPTH_TEST);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
     // build and compile shaders
     // -------------------------
     Shader mushroomShader("resources/shaders/mushroom.vs", "resources/shaders/mushroom.fs");
